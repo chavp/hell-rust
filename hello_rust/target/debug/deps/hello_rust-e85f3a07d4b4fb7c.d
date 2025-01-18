bf0229bcc601d414c34e7597d8e886f0454143e3
Chavp <my.parinya@gmail.com>
2025-01-18 21:11:14 +0700

    hello rust

---
+++ hello_rust/target/debug/deps/hello_rust-e85f3a07d4b4fb7c.d
@@ -0,0 +1,5 @@
+D:\projects\rust\hell-rust\hello_rust\target\debug\deps\hello_rust-e85f3a07d4b4fb7c.exe: src\main.rs
+
+D:\projects\rust\hell-rust\hello_rust\target\debug\deps\hello_rust-e85f3a07d4b4fb7c.d: src\main.rs
+
+src\main.rs:
---
+++ hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+D:\projects\rust\hell-rust\hello_rust\target\debug\hello_rust.exe: D:\projects\rust\hell-rust\hello_rust\src\main.rs
